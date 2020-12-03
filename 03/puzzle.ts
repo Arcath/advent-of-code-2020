1fd0e1ad0fc6b5319cf73ea5df4ebd060845b913
Adam Laycock <adam@arcath.net>
2020-12-03 22:06:35 +0000

    add context to gif encoder to make visuals better

--- 03/puzzle.ts
+++ 03/puzzle.ts
@@ -1,24 +1,31 @@
 import fs from 'fs'
 import path from 'path'
-import {createCanvas} from 'canvas'
+import {createCanvas, CanvasRenderingContext2D} from 'canvas'
+import canvasGifEncoder from 'canvas-gif-encoder'
 
-const {readFile, writeFile} = fs.promises
+const {readFile} = fs.promises
+const {createWriteStream} = fs
 
-const SCREEN = path.join(process.cwd(), '03', 'screen.jpg')
+const SCREEN = path.join(process.cwd(), '03', 'screen-X.gif')
 
 export const main = async () => {
   const INPUT = (await readFile(path.join(process.cwd(), '03', 'input.txt'))).toString()
   const rows = INPUT.split(`\r\n`)
 
-  const part1 = await puzzle(rows, 3, 1)
+  const part1 = await puzzle(rows, 3, 1, true, 0)
 
   console.log(`Trees: ${part1}`)
 
   const t1 = await puzzle(rows, 1, 1)
+  console.log('run 1 done')
   const t2 = part1
+  console.log('run 2 done')
   const t3 = await puzzle(rows, 5, 1)
+  console.log('run 3 done')
   const t4 = await puzzle(rows, 7, 1)
-  const t5 = await puzzle(rows, 1, 2)
+  console.log('run 4 done')
+  const t5 = await puzzle(rows, 1, 2, true, 1)
+  console.log('run 5 done')
 
   const part2 = [t1, t2, t3, t4, t5].reduce((count, n) => {
     return count * n
@@ -38,14 +45,33 @@ export const TREE: Tree = '#'
 export const OPEN: Open = '.'
 
 
-export const puzzle = async (rows: string[], pX: number, pY: number) => {
+export const puzzle = async (rows: string[], pX: number, pY: number, visuals: boolean = false, visN: number = 0) => {
   const map: Map = []
 
   const maxX = (rows.length / pY) * pX
 
-  const draw = async (posX: number, posY: number) => {
+  rows.forEach((row) => {
+    let x = row.length
+    let add = [...row.split('')]
+    while(x < maxX){
+      add = [...add, ...row.split('')]
+      x += row.length
+    }
+
+    map.push(add as any)
+  })
+
+  let encoder: any
+  let context: CanvasRenderingContext2D
+
+  if(visuals){
+    encoder = new canvasGifEncoder(map[0].length, map.length)
+    const stream = createWriteStream(SCREEN.replace('X', visN.toString()))
+    encoder.createReadStream().pipe(stream)
+    encoder.begin()
+
     const canvas = createCanvas(map[0].length, map.length)
-    const context = canvas.getContext('2d')
+    context = canvas.getContext('2d')
 
     map.forEach((row, y) => {
       row.forEach((cell, x) => {
@@ -59,27 +85,21 @@ export const puzzle = async (rows: string[], pX: number, pY: number) => {
       })
     })
 
-    context.fillStyle = 'red'
+    context.save()
+  }
+
+  const draw = async (posX: number, posY: number) => {
+    context.restore()
+
+    context.strokeStyle = 'red'
     context.beginPath()
     context.moveTo(0,0)
     context.lineTo(posX, posY)
     context.stroke()
 
-    const buffer = canvas.toBuffer()
-
-    await writeFile(SCREEN, buffer)
+    encoder.addFrame(context, 250)
   }
 
-  rows.forEach((row) => {
-    let x = row.length
-    let add = [...row.split('')]
-    while(x < maxX){
-      add = [...add, ...row.split('')]
-      x += row.length
-    }
-
-    map.push(add as any)
-  })
 
   let x = 0
   let y = 0
@@ -90,11 +110,17 @@ export const puzzle = async (rows: string[], pX: number, pY: number) => {
       trees += 1
     }
 
+    if(visuals){
+      await draw(x, y)
+    }
+
     y += pY
     x += pX
   }
 
-  await draw(x, y)
+  if(visuals){
+    encoder.end()
+  }
 
   return trees
 }
---
+++ typings.d.ts
@@ -0,0 +1,5 @@
+declare module 'canvas-gif-encoder'{
+  const a: any
+
+  export default a
+}
